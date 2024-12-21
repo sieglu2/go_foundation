afefbf251f6650ff7256f03d14e25b2b87636672
Yufan Lu <myhappyland3113@gmail.com>
2024-12-21 15:28:32 -0800

    backoff

--- retry.go
+++ retry.go
@@ -8,10 +8,14 @@ import (
 	"github.com/cenkalti/backoff"
 )
 
-func DoRetry(ctx context.Context, exec func(timeoutCtx context.Context) error, maxTimes int, execTimeout time.Duration) error {
+func DoRetry(
+	ctx context.Context, execTimeout time.Duration,
+	maxTimes int, backoffWait time.Duration,
+	exec func(timeoutCtx context.Context) error,
+) error {
 	logger := Logger()
 
-	backoffConfig := backoff.WithMaxRetries(backoff.NewConstantBackOff(100*time.Millisecond), uint64(maxTimes))
+	backoffConfig := backoff.WithMaxRetries(backoff.NewConstantBackOff(backoffWait), uint64(maxTimes))
 
 	err := backoff.Retry(func() error {
 		if ctx.Err() == context.DeadlineExceeded || ctx.Err() == context.Canceled {
