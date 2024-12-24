8eb3401e3ff37e0ca86cb64d3115345393aa38ee
Yufan Lu <myhappyland3113@gmail.com>
2024-12-24 09:16:18 -0800

    empty

--- logger.go
+++ logger.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"runtime/debug"
+	"strings"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -12,7 +13,7 @@ import (
 
 var (
 	// globalLogger is global zap logger.
-	globalLogger Logging = NewQuietLogger(getLogLevel())
+	globalLogger Logging = NewSugarLogger(getLogLevel())
 )
 
 func getLogLevel() string {
@@ -45,7 +46,7 @@ func OverrideGlobalLogger(logging Logging) {
 	globalLogger = logging
 }
 
-func NewQuietLogger(lvlStr string) *QuietLogger {
+func NewSugarLogger(lvlStr string) *SugarLogger {
 	// First, define our level-handling logic.
 	globalLevel, err := zapcore.ParseLevel(lvlStr)
 	if err != nil {
@@ -81,59 +82,102 @@ func NewQuietLogger(lvlStr string) *QuietLogger {
 	zapLogger := zap.New(core)
 	zap.RedirectStdLog(zapLogger)
 
-	return &QuietLogger{
+	return &SugarLogger{
 		internal: zapLogger.Sugar(),
 	}
 }
 
-type QuietLogger struct {
+func NewEmptyLogger() *EmptyLogger {
+	return &EmptyLogger{}
+}
+
+type SugarLogger struct {
 	internal *zap.SugaredLogger
 }
 
-func (s *QuietLogger) With(args ...interface{}) Logging {
-	return &QuietLogger{
+func (s *SugarLogger) With(args ...interface{}) Logging {
+	return &SugarLogger{
 		internal: s.internal.With(args...),
 	}
 }
 
-func (s *QuietLogger) Debug(args ...interface{}) {
+func (s *SugarLogger) Debug(args ...interface{}) {
 	s.internal.Debug(args...)
 }
 
-func (s *QuietLogger) Info(args ...interface{}) {
+func (s *SugarLogger) Info(args ...interface{}) {
 	s.internal.Info(args...)
 }
 
-func (s *QuietLogger) Warn(args ...interface{}) {
+func (s *SugarLogger) Warn(args ...interface{}) {
 	s.internal.Warn(append(args, "\nCallstack:\n", GetCallStack()))
 }
 
-func (s *QuietLogger) Error(args ...interface{}) {
+func (s *SugarLogger) Error(args ...interface{}) {
 	s.internal.Error(append(args, "\nCallstack:\n", GetCallStack()))
 }
 
-func (s *QuietLogger) Fatal(args ...interface{}) {
+func (s *SugarLogger) Fatal(args ...interface{}) {
 	s.internal.Fatal(append(args, "\nCallstack:\n", GetCallStack()))
 }
 
-func (s *QuietLogger) Debugf(template string, args ...interface{}) {
+func (s *SugarLogger) Debugf(template string, args ...interface{}) {
 	s.internal.Debugf(template, args...)
 }
 
-func (s *QuietLogger) Infof(template string, args ...interface{}) {
+func (s *SugarLogger) Infof(template string, args ...interface{}) {
 	s.internal.Infof(template, args...)
 }
 
-func (s *QuietLogger) Warnf(template string, args ...interface{}) {
+func (s *SugarLogger) Warnf(template string, args ...interface{}) {
 	s.internal.Warn(appendCallstack(template, args...))
 }
 
-func (s *QuietLogger) Errorf(template string, args ...interface{}) {
+func (s *SugarLogger) Errorf(template string, args ...interface{}) {
 	s.internal.Error(appendCallstack(template, args...))
 }
 
-func (s *QuietLogger) Fatalf(template string, args ...interface{}) {
+func (s *SugarLogger) Fatalf(template string, args ...interface{}) {
 	s.internal.Fatal(appendCallstack(template, args...))
+}
+
+type EmptyLogger struct {
+}
+
+func (s *EmptyLogger) With(args ...interface{}) Logging {
+	return &EmptyLogger{}
+}
+
+func (s *EmptyLogger) Debug(args ...interface{}) {
+}
+
+func (s *EmptyLogger) Info(args ...interface{}) {
+}
+
+func (s *EmptyLogger) Warn(args ...interface{}) {
+}
+
+func (s *EmptyLogger) Error(args ...interface{}) {
+}
+
+func (s *EmptyLogger) Fatal(args ...interface{}) {
+	log.Fatal(args...)
+}
+
+func (s *EmptyLogger) Debugf(template string, args ...interface{}) {
+}
+
+func (s *EmptyLogger) Infof(template string, args ...interface{}) {
+}
+
+func (s *EmptyLogger) Warnf(template string, args ...interface{}) {
+}
+
+func (s *EmptyLogger) Errorf(template string, args ...interface{}) {
+}
+
+func (s *EmptyLogger) Fatalf(template string, args ...interface{}) {
+	log.Fatalf(template, args...)
 }
 
 func GetCallStack() string {
@@ -142,6 +186,6 @@ func GetCallStack() string {
 
 func appendCallstack(template string, args ...interface{}) string {
 	liquidated := fmt.Sprintf(template, args...)
-	liquidated = fmt.Sprintf("%s\nCallstack:\n%s", liquidated, GetCallStack())
-	return liquidated
+	stack := strings.ReplaceAll(GetCallStack(), `\n`, "\n")
+	return fmt.Sprintf("%s\nCallstack:\n%s", liquidated, stack)
 }
