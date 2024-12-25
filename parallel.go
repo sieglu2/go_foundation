0b9eb6f17e57a84625837b3d683321131ed49151
Yufan Lu <myhappyland3113@gmail.com>
2024-12-24 16:21:10 -0800

    errors

--- parallel.go
+++ parallel.go
@@ -8,47 +8,46 @@ import (
 
 func RunInParallel(
 	numWorkers int, sleepIntermittent time.Duration, params chan any,
-	onRun func(any) error, onComplete func() error,
+	onRun func(any) error, onComplete func([]error) error,
 ) error {
 	if numWorkers <= 0 {
 		return fmt.Errorf("number of workers must be positive")
 	}
 
-	// Create a WaitGroup to track when all workers are done
 	var wg sync.WaitGroup
 
-	// Create error channel to collect errors from workers
-	errChan := make(chan error)
+	errChan := make(chan error, numWorkers)
 
-	// Start workers
 	for i := 0; i < numWorkers; i += 1 {
 		wg.Add(1)
 		go func(workerID int) {
 			defer wg.Done()
 
 			for param := range params {
-				defer time.Sleep(sleepIntermittent)
-
 				if err := onRun(param); err != nil {
 					errChan <- fmt.Errorf("worker %d failed processing %+v: %v", workerID, param, err)
-					return
 				}
+
+				time.Sleep(sleepIntermittent)
 			}
 		}(i)
 	}
 
-	// Wait for all workers to finish
-	wg.Wait()
-	close(errChan)
-
-	if err := onComplete(); err != nil {
-		return fmt.Errorf("failed on onComplete: %v", err)
-	}
+	// Start a goroutine to close errChan after all workers finish
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
 
 	// Collect any errors
 	var errors []error
 	for err := range errChan {
 		errors = append(errors, err)
+	}
+
+	// Run onComplete after collecting errors
+	if err := onComplete(errors); err != nil {
+		return fmt.Errorf("failed on onComplete: %v", err)
 	}
 
 	// If there were any errors, return them combined
